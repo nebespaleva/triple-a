8883af541329214c8ac857552b67aed41a855eae
Aleksandr <Aleksandr.kornushin@yandex.ru>
2021-09-29 21:55:08 +0300

    add GeneralPostDescription component

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,7 @@ function App() {
   return (
     <>
 		<Navigation/>
-		<HeaderPost
-			themeTitle='Featured article'
-			postTitle='World’s Most Dangerous Technology Ever Made.'
-			author='Ralph Hawkins'
-			postDate='May 7, 2019'
-			postDescription='Proident aliquip velit qui commodo officia qui consectetur dolor ullamco aliquip elit incididunt. Ea minim ex consectetur excepteur. Ex laborum nostrud mollit sint consectetur Lorem amet aliqua do enim. Commodo duis dolor anim excepteur. In aliquip mollit nulla consequat velit magna.'
-		/>
+		<HeaderPost/>
     </>
   );
 }
--- src/Sections/HeaderPost/index.tsx
+++ src/Sections/HeaderPost/index.tsx
@@ -1,37 +1,19 @@
 import React from "react";
+import GeneralPostDescription from "../../Components/GeneralPostDescription";
 import './style.scss';
 
-interface HeaderPostProps {
-    themeTitle: string;
-    postTitle: string;
-    author: string;
-    postDate: string;
-    postDescription: string;
-}
 
-const HeaderPost = ({ 
-    themeTitle,
-    postTitle,
-    author,
-    postDate,
-    postDescription }: HeaderPostProps) => {
+
+const HeaderPost = () => {
     return(
         <div className='header-post'>
-            <div className='header-post__description'>
-                <h3 className='header-post__description-theme'>{themeTitle}</h3>
-                <h2 className='header-post__description-postTitle'>{postTitle}</h2>
-                <div className='header-post__description-info'>
-                    <span className='header-post__description-info-block'>
-                        {author}
-                    </span>
-                    <span className='header-post__description-info-block'>
-                        {postDate}
-                    </span>
-                </div>
-                <span className='header-post__description-short'>
-                    {postDescription}
-                </span>
-            </div>
+            <GeneralPostDescription
+                themeTitle='Featured article'
+                postTitle='World’s Most Dangerous Technology Ever Made.'
+                author='Ralph Hawkins'
+                postDate='May 7, 2019'
+                postDescription='Proident aliquip velit qui commodo officia qui consectetur dolor ullamco aliquip elit incididunt. Ea minim ex consectetur excepteur. Ex laborum nostrud mollit sint consectetur Lorem amet aliqua do enim. Commodo duis dolor anim excepteur. In aliquip mollit nulla consequat velit magna.'
+            />
         </div>
     )
 }
