e9c8c72de0ef434d4a95f5f82d60e431ea7318df
nebespaleva <aleksandr.kornushin@yandex.ru>
2021-10-23 14:11:39 +0300

    adjusting styles for desctop version

--- src/Pages/AboutMe/AboutMe.tsx
+++ src/Pages/AboutMe/AboutMe.tsx
@@ -1,14 +1,15 @@
-import React from "react";
-import Navigation from "../../Sections/Navigation";
-import { pageProps } from "../Home/Home";
+import '../Home/style.scss';
 
+import Navigation from "../../Sections/Navigation";
+import React from "react";
+import { pageProps } from "../Home/Home";
 
 const AboutMe = ({ active }: pageProps) => {
     return(
-        <>
+        <div className='wrapper'>
             <Navigation active={active}/>
             <div>AboutMe</div>
-        </>
+        </div>
     )
 }
 
